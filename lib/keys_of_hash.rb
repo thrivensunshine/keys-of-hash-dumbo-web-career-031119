ad3c54e313937c176f705850dc4d9fc57b66b0da
Danielle <danib.sgh@gmail.com>
2019-02-28 15:08:49 -0500

    donzo

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,25 @@
+animals ={
+  "sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama","kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar","koala"=>"Australia"
+  }
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*arguments)
+
+  ans = []
+    each do |a,b|
+    arguments.collect do |i|
+    puts i
+    if i == b
+    ans << a
+    end
+    end
+    # ans
   end
+  ans.uniq
+  end
+
 end
